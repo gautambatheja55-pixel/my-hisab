944f00bc3eb5daa547890b73b987298b41d53a2d
Gautam Batheja <gautambatheja55@gmail.com>
2026-06-01 13:08:53 +0530

    Feat: web platform with hosting

---
+++ web_public/script.js
@@ -0,0 +1,44 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+ 
+import {
+    getDatabase,
+    ref,
+    push
+} from
+"https://www.gstatic.com/firebasejs/12.14.0/firebase-database.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCfisFUVcgE39J7Fl6DZsngv3SyJb3pn-8",
+    authDomain: "incog-c7772.firebaseapp.com",
+    databaseURL: "https://incog-c7772-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "incog-c7772",
+    storageBucket: "incog-c7772.firebasestorage.app",
+    messagingSenderId: "987101017989",
+    appId: "1:987101017989:web:fa84da5d8fc564b0e6f6ec"
+  };
+
+const recipient=window.location.pathname.split("/")[2];
+const app = initializeApp(firebaseConfig);
+const db=getDatabase(app);
+const recipient=fakePath.split("/")[2];
+const sendBtn = document.getElementById("sendBtn");
+const messageInput=document.getElementById("message");
+
+sendBtn.addEventListener("click",() => {
+    const messageText=messageInput.value;
+    if (messageText.trim()===""){
+        alert("Please type something before sending");
+        return;
+    }
+    
+    push(ref(db,"messages/" + recipient),{
+        text:messageText,
+        time:Date.now()
+    }).then(() => {
+        messageInput.value="";
+        alert("Anonymous message sent successfully");
+    }).catch((error) => {
+        console.error("Database Error: ",error);
+    });
+});
